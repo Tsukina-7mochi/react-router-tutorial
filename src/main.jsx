94e32835c9dfab1e90f701b3d1d3866c93163edf
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-10-08 00:35:15 +0900

    feat: add loader on root component

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import { ErrorPage } from './error-page';
 import { Contact } from './routes/contact';
-import { Root } from './routes/root';
+import { Root, loader as rootLoader } from './routes/root';
 
 import './index.css';
 
@@ -13,6 +13,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: 'contacts/:contactId',
